1c7329d161a34d5f68aaf352680e7da9c1090e21
Triinu Liis Kelder <triinuliis7@gmail.com>
2020-04-10 23:58:21 +0300

    Material-UI integration

--- front/src/routes/Dashboard/Main.js
+++ front/src/routes/Dashboard/Main.js
@@ -1,10 +1,23 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, Fragment } from "react";
 import axios from "../../utils/axios";
 import moment from "moment";
 import { Link } from "react-router-dom";
 import { FormattedMessage, injectIntl } from "react-intl";
 import { RequestOptionsContext } from "./DashboardWrapper";
-
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuList from '@material-ui/core/MenuList';
 import CustomButton from "../../components/CustomButton/CustomButton";
 import CustomInput from "../../components/CustomInput/CustomInput";
 import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
@@ -58,6 +71,35 @@ function Main(props) {
   useEffect(() => {
     getTickets();
   }, []);
+
+  //TOGGLE-START
+
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleClick = () => {
+    openModal(dropdownOptions[selectedIndex])
+  };
+
+  const handleMenuItemClick = (event, index) => {
+    setSelectedIndex(index);
+    setOpen(false);
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  //TOGGLE-END
 
   function handleInputChange({ target }) {
     setTicketDetails({
@@ -165,155 +207,230 @@ function Main(props) {
   const selectedCategory = getSelectedCategory(ticketDetails.category);
 
   return (
-    <div className="dashboard-children main">
-      <div className="dashboard-children-wrapper">
-        <DashboardNavigation />
-        {isModalOpen && (
-          <TaskModal
-            onClose={() => closeModal()}
-            modalType={modalType}
-            handleBtnClick={(comment) => handleBtnClick(comment)}
-            showCommentField={modalType === "resolve" || modalType === "reject"}
-          />
-        )}
-
-        <div className="tickets">
-          <div className="ticket-list">
-            {/* FILTERS */}
-            <div className="filter-list">
-              <h5>
-                <FormattedMessage id="filters" />
-              </h5>
-              <div className="filters">
-                {FILTER_KEYS.map((filter) => (
-                  <CustomInput
-                    labelId={filter}
-                    name={filter}
-                    modifier="secondary"
-                    layout="checkbox"
-                    checked={filterStates[filter]}
-                    onChange={handleFilters}
-                  />
-                ))}
-              </div>
+    <Fragment>
+      <Grid container spacing={0}>
+        <Grid item xs={12}>
+          <DashboardNavigation />
+          {isModalOpen && (
+            <TaskModal
+              onClose={() => closeModal()}
+              modalType={modalType}
+              handleBtnClick={(comment) => handleBtnClick(comment)}
+              showCommentField={modalType === "resolve" || modalType === "reject"}
+            />
+          )}
+          <div className="filter-list">
+            <h5>
+              <FormattedMessage id="filters" />
+            </h5>
+            <div className="filters">
+              {FILTER_KEYS.map((filter) => (
+                <CustomInput
+                  labelId={filter}
+                  name={filter}
+                  modifier="secondary"
+                  layout="checkbox"
+                  checked={filterStates[filter]}
+                  onChange={handleFilters}
+                />
+              ))}
             </div>
-            {/* END FILTERS */}
-
-            {/* TICKETS */}
-            <div className="ticket-list-wrapper">
-              {isLoadingTickets ? (
-                <LoadingSpinner />
-              ) : (
-                tickets
-                  .filter(ticketFilters)
-                  .map((ticket) => <Ticket {...ticket} />)
-              )}
-            </div>
-            {/* END TICKETS */}
+            <Button
+              variant="contained"
+              color="default"
+              onClick={() => createTask()}
+            >
+              <FormattedMessage id="modal.approve" />
+            </Button>
           </div>
-
+        </Grid>
+        <Grid item xs={6}>
+          <div className="ticket-list-wrapper">
+            {isLoadingTickets ? (
+              <LoadingSpinner />
+            ) : (
+              tickets
+                .filter(ticketFilters)
+                .map((ticket) => <Ticket {...ticket} />)
+            )}
+          </div>
+        </Grid>
+        <Grid item xs={6}>
           <div className="task-manager">
-            <div className="flex-end action-wrapper">
-              <CustomButton
-                titleId="newTask"
-                modifier="secondary"
-                onClick={createTask}
-              />
-            </div>
-
             <div className="task-manager-wrapper">
               <div className="input-container">
-                <CustomInput
-                  labelId="request"
+                <TextField
+                  className="input"
+                  id="request"
                   name="request"
-                  modifier="secondary"
-                  layout="textarea"
+                  label="request"
+                  variant="outlined"
                   value={ticketDetails.request}
                   onChange={handleInputChange}
                 />
-
-                <CustomInput
-                  labelId="requesterName"
+                <TextField
+                  className="input"
+                  id="requesterName"
                   name="name"
-                  modifier="secondary"
+                  label="requesterName"
+                  variant="outlined"
                   value={ticketDetails.name}
                   onChange={handleInputChange}
                 />
-
-                <CustomInput
-                  labelId="category"
-                  name="category"
-                  modifier="secondary"
-                  layout="select"
-                  onChange={handleInputChange}
+                <FormControl
+                  className="input"
+                  variant="outlined"
                 >
-                  <option value="" />
-                  {categories.map((option) => (
-                    <option
-                      value={option._id}
-                      key={option._id}
-                      selected={option._id === ticketDetails.category}
-                    >
-                      {option.name}
-                    </option>
-                  ))}
-                </CustomInput>
-
-                <CustomInput
-                  labelId="phone"
+                  <InputLabel id="demo-simple-select-outlined-label">
+                    <FormattedMessage id="category" />
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-outlined-label"
+                    id="demo-simple-select-outlined"
+                    name="category"
+                    value={ticketDetails.category}
+                    onChange={handleInputChange}
+                    label="category"
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    {categories.map((option) => (
+                      <MenuItem value={option._id}>
+                        {option.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                <TextField
+                  className="input"
+                  id="phone"
                   name="phone"
-                  modifier="secondary"
+                  label="phone"
+                  variant="outlined"
                   value={ticketDetails.phone}
                   onChange={handleInputChange}
                 />
 
                 {selectedCategory && selectedCategory.needsAddress && (
-                  <CustomInput
-                    labelId="address"
+                  <TextField
+                    className="input"
+                    id="address"
                     name="address"
-                    modifier="secondary"
+                    label="address"
+                    variant="outlined"
                     value={ticketDetails.address}
                     onChange={handleInputChange}
                   />
                 )}
 
-                <CustomInput
-                  labelId="area"
-                  name="area"
-                  modifier="secondary"
-                  layout="select"
-                  onChange={handleInputChange}
+                <FormControl
+                  className="input"
+                  variant="outlined"
                 >
-                  <option value="" />
-                  {areas.map((option) => (
-                    <option
-                      value={option._id}
-                      key={option._id}
-                      selected={option._id === ticketDetails.area}
-                    >
-                      {option.name}
-                    </option>
-                  ))}
-                </CustomInput>
-
-                <CustomInput
-                  labelId="assignee"
-                  name="assignee"
-                  modifier="secondary"
-                  layout="select"
-                  onChange={handleInputChange}
+                  <InputLabel id="demo-simple-select-outlined-label">
+                    <FormattedMessage id="area" />
+                  </InputLabel>
+                  <Select
+                    labelId="area"
+                    id="area"
+                    name="area"
+                    value={ticketDetails.area}
+                    onChange={handleInputChange}
+                    label="area"
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    {areas.map((option) => (
+                      <MenuItem value={option._id}>
+                        {option.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                <FormControl
+                  className="input"
+                  variant="outlined"
                 >
-                  <option value="" />
-                  {users.map((option) => (
-                    <option
-                      value={option._id}
-                      key={option._id}
-                      selected={option._id === ticketDetails.user}
+                  <InputLabel id="demo-simple-select-outlined-label">
+                    <FormattedMessage id="assignee" />
+                  </InputLabel>
+                  <Select
+                    labelId="assignee"
+                    id="assignee"
+                    value={ticketDetails.user}
+                    onChange={handleInputChange}
+                    label="assignee"
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    {users.map((option) => (
+                      <MenuItem value={option._id}>
+                        {option.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                <Grid container spacing={0}>
+                  <Grid item xs={6}>
+                    <Grid container direction="column" alignItems="center">
+                      <Grid item xs={12}>
+                        <ButtonGroup variant="contained" color="primary" ref={anchorRef} aria-label="split button">
+                          <Button onClick={handleClick}>{dropdownOptions[selectedIndex]}</Button>
+                          <Button
+                            color="primary"
+                            size="small"
+                            aria-controls={open ? 'split-button-menu' : undefined}
+                            aria-expanded={open ? 'true' : undefined}
+                            aria-label="select merge strategy"
+                            aria-haspopup="menu"
+                            onClick={handleToggle}
+                          >
+                            <ArrowDropDownIcon />
+                          </Button>
+                        </ButtonGroup>
+                        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                          {({ TransitionProps, placement }) => (
+                            <Grow
+                              {...TransitionProps}
+                              style={{
+                                transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+                              }}
+                            >
+                              <Paper>
+                                <ClickAwayListener onClickAway={handleClose}>
+                                  <MenuList id="split-button-menu">
+                                    {dropdownOptions.map((option, index) => (
+                                      <MenuItem
+                                        key={option}
+                                        disabled={index === 2}
+                                        selected={index === selectedIndex}
+                                        onClick={(event) => handleMenuItemClick(event, index)}
+                                      >
+                                        {option}
+                                      </MenuItem>
+                                    ))}
+                                  </MenuList>
+                                </ClickAwayListener>
+                              </Paper>
+                            </Grow>
+                          )}
+                        </Popper>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => openModal("approve")}
                     >
-                      {option.firstName} {option.lastName}
-                    </option>
-                  ))}
-                </CustomInput>
+                      <FormattedMessage id="modal.approve" />
+                    </Button>
+                  </Grid>
+                </Grid>
 
                 <div className="flex-end action-wrapper">
                   <CustomInput
@@ -330,18 +447,21 @@ function Main(props) {
                       </option>
                     ))}
                   </CustomInput>
-                  <CustomButton
-                    titleId="modal.approve"
-                    modifier="primary"
+
+                  <Button
+                    variant="contained"
+                    color="primary"
                     onClick={() => openModal("approve")}
-                  />
+                  >
+                    <FormattedMessage id="modal.approve" />
+                  </Button>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+        </Grid>
+      </Grid>
+    </Fragment>
   );
 }
 
---
+++ front/src/routes/Dashboard/Ticket.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState, useContext } from "react";
+import { useFormikContext } from "formik";
+import CustomButton from "../../components/CustomButton/CustomButton";
+import { FormattedMessage } from "react-intl";
+import moment from "moment";
+import history from "../../utils/history";
+import { RequestOptionsContext } from "./RequestWrapper";
+
+function Ticket({ ticket, selectTicket }) {
+  const date = new moment(ticket.createdAt).format("DD.MM.YY");
+  const displayedDate = date !== "invalid date" ? date : "";
+
+  return (
+    <div onClick={() => selectTicket()} className="ticket">
+      <div className="ticket-wrapper">
+        <h5>{ticket.request}</h5>
+
+        <div className="footer">
+          <h5>{displayedDate}</h5>
+          <h5>{ticket.category}</h5>
+          <h5>{ticket.area}</h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ticket;
