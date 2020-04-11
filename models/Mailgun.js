73087ccbdb12d8874884baba9c032c2d73d4a041
Viktor Lillemäe <viktor@zelos-app.com>
2020-04-11 05:35:19 +0000

    fix outgoing emails

--- models/Mailgun.js
+++ models/Mailgun.js
@@ -1,24 +1,30 @@
-const axios = require('axios');
+const axios = require("axios");
 
 class Mailgun {
-    constructor(email) {
-        const key = Buffer.from(`api:${process.env.MAILGUN_API_KEY}`).toString('base64').toString('utf8');
-        this.headers = {
-            Authorization: `Basic ${key}`
-        }
-        this.params = {
-            from: `${process.env.MAILGUN_FROM_NAME} <${process.env.MAILGUN_FROM_EMAIL}>`,
-            to: email
-        }
-    }
-    async send(subject, text) {
-        this.params.subject = subject;
-        this.params.text = text;
-        axios.post(`${process.env.MAILGUN_BASE_URL}/${process.env.MAILGUN_DOMAIN}/messages`, null, {
-            params: this.params,
-            headers: this.headers
-        })
-    }
+  constructor(email) {
+    const key = Buffer.from(`api:${process.env.MAILGUN_API_KEY}`)
+      .toString("base64")
+      .toString("utf8");
+    this.headers = {
+      Authorization: `Basic ${key}`,
+    };
+    this.params = {
+      from: `${process.env.EMAIL_FROM_NAME} <${process.env.EMAIL_FROM_EMAIL}>`,
+      to: email,
+    };
+  }
+  async send(subject, text) {
+    this.params.subject = subject;
+    this.params.text = text;
+    axios.post(
+      `${process.env.MAILGUN_BASE_URL}/${process.env.MAILGUN_DOMAIN}/messages`,
+      null,
+      {
+        params: this.params,
+        headers: this.headers,
+      }
+    );
+  }
 }
 
-module.exports = Mailgun
+module.exports = Mailgun;
