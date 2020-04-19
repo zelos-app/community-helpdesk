9a2acc1b1ae939ce9cddc05b6a07ae9e2ed0f4ff
Stepan Bolotnikov <stepan.bolotnikov@proekspert.ee>
2020-04-19 12:26:13 +0300

    Cherry-picked changes from pr#59

--- src/routes/Request/Category.js
+++ src/routes/Request/Category.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext } from "react";
 import { useFormikContext } from "formik";
-import CustomButton from "../../components/CustomButton/CustomButton";
 import { FormattedMessage } from "react-intl";
 import history from "../../utils/history";
 import { RequestOptionsContext } from "./RequestWrapper";
@@ -32,33 +31,38 @@ function Category() {
   };
 
   return (
-    <div className="request-children category">
-      <div className="request-children-wrapper">
-        <div className="text-wrapper">
-          <h1 className="text-alpha">
-            <FormattedMessage id="categoryHead" />
-          </h1>
-          <h3 className="text-alpha">
-            <FormattedMessage
-              id="categoryBody"
-              values={{ p: (...chunks) => <p>{chunks}</p> }}
-            />
-          </h3>
-        </div>
+    <Grid container spacing={0}>
+      <Grid item xs={5}>
+        <div className="illustration" />
+      </Grid>
+      <Grid item xs={7}>
+        <div className="request-children-wrapper">
+          <div className="text-wrapper">
+            <h1 className="text-alpha">
+              <FormattedMessage id="categoryHead" />
+            </h1>
+            <h3 className="text-alpha">
+              <FormattedMessage
+                id="categoryBody"
+                values={{ p: (...chunks) => <p>{chunks}</p> }}
+              />
+            </h3>
+          </div>
 
-        <Grid container direction="row" justify="center" spacing={2}>
-          <SelectorButtons />
-        </Grid>
-        <div className="action-wrapper">
-          <FormattedMessage id="or" />
+          <Grid container direction="row" justify="center" spacing={2}>
+            <SelectorButtons />
+          </Grid>
+          <div className="action-wrapper">
+            <FormattedMessage id="or" />
+          </div>
+          <div className="action-wrapper">
+            <Button onClick={() => history.go(-1)}>
+              <FormattedMessage id="back" />
+            </Button>
+          </div>
         </div>
-        <div className="action-wrapper">
-          <Button onClick={() => history.go(-1)}>
-            <FormattedMessage id="back" />
-          </Button>
-        </div>
-      </div>
-    </div>
+      </Grid>
+    </Grid>
   );
 }
 
--- src/routes/Request/Intro.js
+++ src/routes/Request/Intro.js
@@ -2,33 +2,38 @@ import React from "react";
 import CustomButton from "../../components/CustomButton/CustomButton";
 import { FormattedMessage } from "react-intl";
 import { Link } from "react-router-dom";
+import { Grid } from "@material-ui/core";
 
 function Intro() {
   return (
-    <div className="request">
-      <div className="request-wrapper">
-        <div className="request-children intro">
-          <div className="request-children-wrapper">
-            <div className="text-wrapper">
-              <h1 className="text-alpha">
-                <FormattedMessage id="introHead" />
-              </h1>
-              <h3 className="text-alpha">
-                <FormattedMessage id="introBody" values={{p: (...chunks) => <p>{chunks}</p>}} />
-              </h3>
-            </div>
-            <div className="action-wrapper">
-              <Link to="/request">
-                <CustomButton titleId="iNeedHelp" modifier="primary" />
-              </Link>
-              <Link to="/app">
-                <CustomButton titleId="iWantToHelp" modifier="secondary" />
-              </Link>
-            </div>
+    <Grid container spacing={0}>
+      <Grid item xs={5}>
+        <div className="illustration" />
+      </Grid>
+      <Grid item xs={7}>
+        <div className="request-children-wrapper">
+          <div className="text-wrapper">
+            <h1 className="text-alpha">
+              <FormattedMessage id="introHead" />
+            </h1>
+            <h3 className="text-alpha">
+              <FormattedMessage
+                id="introBody"
+                values={{ p: (...chunks) => <p>{chunks}</p> }}
+              />
+            </h3>
+          </div>
+          <div className="action-wrapper">
+            <Link to="/request">
+              <CustomButton titleId="iNeedHelp" modifier="primary" />
+            </Link>
+            <Link to="/app">
+              <CustomButton titleId="iWantToHelp" modifier="secondary" />
+            </Link>
           </div>
         </div>
-      </div>
-    </div>
+      </Grid>
+    </Grid>
   );
 }
 
