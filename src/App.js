37e137bf39eb7fc0e01c61a6eaeeaa96eebd9cc6
Raner Piibur <u321934@danfoss.com>
2020-04-19 19:24:35 +0300

    Added todo.

--- src/App.js
+++ src/App.js
@@ -40,6 +40,7 @@ export default () => {
       defaultLocale={i18nConfig.defaultLocale}
       messages={i18nConfig.messages[locale]}
     >
+      // TODO: This throws error I suppose because when we started moving to Material UI it want the MUI theme provider. This is styled.
       <ThemeProvider theme={{ ...lightTheme, ...variables }}>
         <LoggedInContext.Provider value={{ data: isLogged, set: setLogged }}>
           {/* Include global styles */}
