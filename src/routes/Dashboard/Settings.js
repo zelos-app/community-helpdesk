7665f5eeffa371016cb16441cce39ecbd4b10104
Raner Piibur <u321934@danfoss.com>
2020-04-19 14:38:25 +0300

    Removed dashboard navigation.

--- src/routes/Dashboard/Settings.js
+++ src/routes/Dashboard/Settings.js
@@ -1,11 +1,9 @@
 import React from "react";
-import DashboardNavigation from "../../components/DashboardNavigation/DashboardNavigation";
 import ListPopulationComponent from "../../components/SettingsPage/ListPopulationComponent";
 
 export default function Settings(props) {
   return (
     <div className="dashboard-children settings">
-      <DashboardNavigation/>
       <ListPopulationComponent/>
     </div>
   );
