b38f3a926a4117f66f55cd0019090f011c960b7d
Viktor Lillemäe <viktor@zelos-app.com>
2020-04-23 16:28:12 +0000

    gotta hit save before git add

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function config() {
-     find "/etc/nginx" -maxdepth 5 -type f -exec sed -i -e 's|{{APP_DOMAIN}}|'"${APP_DOMAIN}"'|g' {} \;
+     find "/etc/nginx" -maxdepth 5 -type f -exec sed -i -e 's|{{APP_HOST}}|'"${APP_HOST}"'|g' {} \;
      find "/etc/nginx" -maxdepth 5 -type f -exec sed -i -e 's|{{API_HOST}}|'"${API_HOST}"'|g' {} \;
 }
 
