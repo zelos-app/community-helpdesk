98dfa0bf03466815b0592e3bd790195e47a5918c
Viktor Lillemäe <viktor@zelos-app.com>
2020-04-23 16:11:38 +0000

    modify env settings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 function config() {
      find "/etc/nginx" -maxdepth 5 -type f -exec sed -i -e 's|{{APP_DOMAIN}}|'"${APP_DOMAIN}"'|g' {} \;
-     find "/etc/traefik" -maxdepth 5 -type f -exec sed -i -e 's|{{APP_DOMAIN}}|'"${APP_DOMAIN}"'|g' {} \;
+     find "/etc/nginx" -maxdepth 5 -type f -exec sed -i -e 's|{{API_HOST}}|'"${API_HOST}"'|g' {} \;
 }
 
 config
