33eaaadae0b0df843a9f6717bd9856ec6f704c0a
Yana Banina <ybanina@axmit.com>
2020-04-26 12:33:12 +0300

    Second form for /settings/zelos

--- src/routes/Dashboard/Zelos.js
+++ src/routes/Dashboard/Zelos.js
@@ -7,6 +7,8 @@ import FormInput from "../../components/CustomInput/FormInput";
 import CustomButton from "../../components/CustomButton/CustomButton";
 import { FormattedMessage } from "react-intl";
 import InputAdornment from "@material-ui/core/InputAdornment";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 export const Zelos = () => {
   const [info, setInfo] = useState();
@@ -31,7 +33,7 @@ export const Zelos = () => {
     <>
       <Grid container alignItems="center" spacing={3}>
         <Grid item xs={12}>
-          <Typography gutterBottom variant="h4">
+          <Typography gutterBottom variant="h5">
             <FormattedMessage id={"connection"} />
           </Typography>
         </Grid>
@@ -74,6 +76,88 @@ export const Zelos = () => {
                   name="password"
                   label={"locale.fieldZelosPassword"}
                   type={"password"}
+                />
+              </div>
+
+              <div className="category-action">
+                <Field>
+                  {({ form }) => (
+                    <CustomButton
+                      titleId="save"
+                      modifier="primary"
+                      type="submit"
+                      disabled={form.isSubmitting || !form.isValid}
+                    />
+                  )}
+                </Field>
+              </div>
+            </Form>
+          </Formik>
+        </Grid>
+      </Grid>
+
+      <Grid container alignItems="center" spacing={3}>
+        <Grid item xs={12}>
+          <Typography gutterBottom variant="h5">
+            <FormattedMessage id={"settings"} />
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <Formik
+            initialValues={{
+              confirmAssignment: info?.confirmAssignment || false,
+              confirmCompletion: info?.confirmCompletion || false,
+              safetyWarning: info?.safetyWarning || "",
+            }}
+            onSubmit={async (values, formik) => {
+              try {
+                await axios.put("/api/settings/zelos", { ...info, ...values });
+
+                formik.setSubmitting(false);
+              } catch (e) {
+                alert(e.message);
+              }
+            }}
+          >
+            <Form>
+              <div className="input-container">
+                <FormInput
+                  name="safetyWarning"
+                  label={"safetyWarning"}
+                  multiline
+                />
+                <FormControlLabel
+                  className={"mt-10"}
+                  control={
+                    <Field name="confirmCompletion">
+                      {({ field }) => (
+                        <Checkbox
+                          color="primary"
+                          name={field.name}
+                          checked={field.value}
+                          {...field}
+                        />
+                      )}
+                    </Field>
+                  }
+                  label={<FormattedMessage id="confirmCompletion" />}
+                />
+                <FormControlLabel
+                  className={"mt-10"}
+                  control={
+                    <Field name="confirmAssignment">
+                      {({ field }) => (
+                        <Checkbox
+                          color="primary"
+                          name={field.name}
+                          checked={field.value}
+                          {...field}
+                        />
+                      )}
+                    </Field>
+                  }
+                  label={<FormattedMessage id="confirmAssignment" />}
                 />
               </div>
 
