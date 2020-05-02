32f3ecf3dbf7f768505c9b0ff5a1f7453d3368db
Raner Piibur <u321934@danfoss.com>
2020-05-02 22:36:43 +0300

    Disavbed horizontal gutter foe the container. Removed the margin from the illustration. Removed divs from the Request page. Now using grid, box. This fixes the different screen sizes strange behaviour.

--- src/routes/Request/Request.js
+++ src/routes/Request/Request.js
@@ -3,8 +3,22 @@ import { FormattedMessage, useIntl } from "react-intl";
 import { useFormikContext, Field } from "formik";
 import history from "../../utils/history";
 import { TextField, Button, Grid } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 function Request() {
+  const classes = useStyles();
+
   const { values } = useFormikContext();
   const intl = useIntl();
 
@@ -24,10 +38,11 @@ function Request() {
 
   return (
     <Grid container spacing={0}>
-      <Grid item xs={5}>
+      <Grid item md={5} xs={12}>
         <div className="illustration" />
       </Grid>
-      <Grid item xs={7} className="right-block">
+
+      <Grid item md={7} xs={12}>
         <Grid
           container
           spacing={0}
@@ -36,42 +51,47 @@ function Request() {
           justify="center"
           style={{ minHeight: "100vh" }}
         >
-          <div className="request-children-wrapper">
-            <div className="text-wrapper">
+          <Grid
+            container
+            justify="center"
+            alignItems="center"
+            direction="column"
+          >
+            <Box mx={1}>
               <h1 className="text-alpha">
                 <FormattedMessage id="writeRequest.content.header" />
               </h1>
-            </div>
 
-            <Field
-              name="request"
-              as={TextField}
-              label={<FormattedMessage id="writeRequest.content.body" />}
-              placeholder={intl.formatMessage({
-                id: "writeRequest.placeholders.request",
-              })}
-              variant="outlined"
-              multiline
-              rows="5"
-              required
-              fullWidth
-            />
+              <Field
+                name="request"
+                as={TextField}
+                label={<FormattedMessage id="writeRequest.content.body" />}
+                placeholder={intl.formatMessage({
+                  id: "writeRequest.placeholders.request",
+                })}
+                variant="outlined"
+                multiline
+                rows="5"
+                required
+                fullWidth
+              />
 
-            <div className="action-wrapper">
-              <Button type="button" onClick={back} variant="contained">
-                <FormattedMessage id="writeRequest.buttons.back" />
-              </Button>
-              <Button
-                color="primary"
-                onClick={next}
-                type="button"
-                disabled={!values.request}
-                variant="contained"
-              >
-                <FormattedMessage id="writeRequest.buttons.next" />
-              </Button>
-            </div>
-          </div>
+              <div className={classes.root}>
+                <Button type="button" onClick={back} variant="contained">
+                  <FormattedMessage id="writeRequest.buttons.back" />
+                </Button>
+                <Button
+                  color="primary"
+                  onClick={next}
+                  type="button"
+                  disabled={!values.request}
+                  variant="contained"
+                >
+                  <FormattedMessage id="writeRequest.buttons.next" />
+                </Button>
+              </div>
+            </Box>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
